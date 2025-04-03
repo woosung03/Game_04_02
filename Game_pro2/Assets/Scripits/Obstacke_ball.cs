3944477aa1febb699839493c09eacd3a2c145f5c
woosung03 <woosung03>
2025-04-03 19:58:17 +0900

    add obstacle

---
+++ Game_pro2/Assets/Scripits/Obstacke_ball.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Obstacke_ball : MonoBehaviour
+{
+    public GameObject prefabBalls; //Ball Object push
+    Transform ObBalls;
+    void Start()
+    {
+        ObBalls = GetComponent<Transform>( );
+
+        StartCoroutine(ObstackeBalls( ));
+    }
+
+   
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ObstackeBalls()
+    {
+        while (true)
+        {
+            Instantiate(prefabBalls, ObBalls.position, Quaternion.identity);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
