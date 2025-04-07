c2c2e55821ea1ef794607943b7c6e11e94647745
woosung03 <woosungkang2003@gmial.com>
2025-04-07 21:40:55 +0900

    플레이어 방해 오브젝트 생성 로직 생각중

---
+++ Game_pro2/Assets/Scripits/ObDIsroy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObDIsroy : MonoBehaviour //time up ball disroy
+{
+    Transform ObBalls;
+  
+    public float speed;
+
+    void Start()
+    {
+        ObBalls = GetComponent<Transform>(); //ObBall position
+        float randomTime = Random.Range(5.0f, 10.0f); // 랜덤한 시간 설정 (5초 ~ 10초 사이)
+        speed = Random.Range(2.0f, 10.0f); // 랜덤한 스퓨ㅣ드 설정 (0.05f ~ 1.0f)
+        StartCoroutine(DestroySelf(randomTime));  //Obstacle ball destroy
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ObBalls.Translate(Vector2.right * speed * Time.deltaTime); //ObBall move speed
+
+    }
+    IEnumerator DestroySelf(float delay)
+    {
+        yield return new WaitForSeconds(delay); // 랜덤 시간 후 파괴
+        Destroy(this.gameObject);
+    }
+}
--- Game_pro2/Assets/Scripits/Obstacke_ball.cs
+++ Game_pro2/Assets/Scripits/Obstacke_ball.cs
@@ -14,6 +14,7 @@ public class Obstacke_ball : MonoBehaviour
         StartCoroutine(ObstackeBalls( ));
     }
 
+
    
 
 
